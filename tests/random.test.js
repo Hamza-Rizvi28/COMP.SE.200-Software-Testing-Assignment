ea222712a3127fcec02d66caf56e35a7cf4871ac
Hamza Rizvi <hamza.rizvi@tuni.fi>
2023-12-11 15:27:01 +0200

    updated test script to test random test run

---
+++ tests/random.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+
+describe("Random test", () => {
+    it('returns null', () => {
+        let res = "Random Test";
+        expect(res).to.equals("Random Test")
+    })
+});
